ff3483e1a73abfec3abaf618d57fbd739b018522
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-04-08 19:52:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from "react";
-import MarkdownEditor from "./components/MarkdownEditor";
-import "./../styles/App.css";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
-const App = () => {
+function Markdown() {
   const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const changeLoadingVar = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setTimeout(changeLoadingVar, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
-    <div className="markdown-container">
-      <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+    <div className="app">
+      <textarea className="textarea" onChange={handleChange} />
+      <ReactMarkdown className="preview">{markdown}</ReactMarkdown>
     </div>
   );
-};
+}
 
-export default App;
+export default Markdown;
