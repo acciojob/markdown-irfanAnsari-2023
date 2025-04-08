aa6c83bd35d5690aa4ac6773ad6179e60480cca3
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-04-08 19:53:54 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
+// import React from "react";
+// import ReactMarkdown from "react-markdown";
 
-const MarkdownEditor = ({ markdown, setMarkdown }) => {
-  return (
-    <div className="editor-container">
-      <textarea
-        className="markdown-input"
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-        placeholder="Type Markdown here..."
-      />
-      <div className="markdown-preview">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
-      </div>
-    </div>
-  );
-};
+// const MarkdownEditor = ({ markdown, setMarkdown }) => {
+//   return (
+//     <div className="editor-container">
+//       <textarea
+//         className="markdown-input"
+//         value={markdown}
+//         onChange={(e) => setMarkdown(e.target.value)}
+//         placeholder="Type Markdown here..."
+//       />
+//       <div className="markdown-preview">
+//         <ReactMarkdown>{markdown}</ReactMarkdown>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default MarkdownEditor;
+// export default MarkdownEditor;
